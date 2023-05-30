723199a374882995af8d3fa2cd2e881a2827c0d3
anastasis01 <100617025+anastasis01@users.noreply.github.com>
2023-05-31 00:58:40 +0300

    Add files via upload

---
+++  gr.upatras.junit.first/src/main/java/gr/upatras/junit/first/MyMain.java
@@ -0,0 +1,8 @@
+package gr.upatras.junit.first;
+
+public class MyMain {
+    public static void main(String[] args) {
+    	MyClass m=new MyClass();
+    	m.sub(7, 5);
+    }
+}
---
+++  gr.upatras.junit.first/src/test/java/gr/upatras/junit/first/MyClassTest.java
@@ -0,0 +1,14 @@
+package gr.upatras.junit.first;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyClassTest {
+
+	@Test
+	void testSub() {
+		fail("Not yet implemented");
+	}
+
+}
